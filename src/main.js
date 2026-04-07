bbfa603f64c1d9b01702656a821faedadaf6c27e
Hasim Febri <ajenggeger@gmail.com>
2026-04-07 10:51:53 +0700

    tr

--- src/main.js
+++ src/main.js
@@ -1,19 +1,22 @@
-import {home} from "./src/js/home.js";
-import {bride} from "./src/js/bride.js";
-import {time} from "./src/js/time.js";
-import {galeri} from "./src/js/galeri.js";
-import {wishas} from "./src/js/wishas.js";
-import {navbar} from "./src/js/navbar.js";
-import {welcome} from "./src/js/welcome.js";
+// src/main.js - dengan path yang benar
+import {home} from "./js/home.js";
+import {bride} from "./js/bride.js";
+import {time} from "./js/time.js";
+import {galeri} from "./js/galeri.js";
+import {wishas} from "./js/wishas.js";
+import {navbar} from "./js/navbar.js";
+import {welcome} from "./js/welcome.js";
 
 // load content
 document.addEventListener('DOMContentLoaded', () => {
-    AOS.init();
-
+    if (typeof AOS !== 'undefined') {
+        AOS.init();
+    }
+    
     welcome();
     navbar();
     home();
-    bride()
+    bride();
     time();
     galeri();
     wishas();
